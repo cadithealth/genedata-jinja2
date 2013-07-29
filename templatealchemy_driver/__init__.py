0a5904808c0015b31eb279e9a4edd061923098a5
grabner <pjg.github@ubergrabner.net>
2013-07-29 14:06:22 -0400

    updated for new namespace module "templatealchemy_driver"

--- templatealchemy_driver/__init__.py
+++ templatealchemy_driver/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
 # file: $Id$
-# lib:  templatealchemy
+# lib:  templatealchemy_driver
 # auth: Philip J Grabner <grabner@cadit.com>
 # date: 2013/07/03
 # copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
@@ -11,11 +11,6 @@ try:
   __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
   __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
-try:
-  from templatealchemy.engine import *
-except ImportError:
-  pass
 
 #------------------------------------------------------------------------------
 # end of $Id$
